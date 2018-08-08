8ebbadb85a27bf3ca26c5eb36bbfc33583f8e7eb
linq2js <linqtojs@gmail.com>
2018-08-09 02:22:40 +0700

    Add typescript support

--- immhelper.d.ts
+++ immhelper.d.ts
@@ -1,6 +1,6 @@
 declare namespace ImmHelper {
     // tuple [selector, action, ...args]
-    type UpdateSpec<T> = [(model: T) => any, string | Function, any[]];
+    type UpdateSpec<T> = [(model: T) => any, string | Function, ...any[]];
     interface Update {
         <T>(model: T, ...specs: UpdateSpec<T>[]): T;
 
